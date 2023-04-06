0135d0be89434c715b1cc7cfc22bd41abe27c608
raz_taz <98036348+raz-taz@users.noreply.github.com>
2023-04-06 21:28:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
-const PORT = 4001;
+const PORT = process.env.PORT || 5000;
 const password = "password123"
 
 const http = require('http').Server(app);
