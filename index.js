bd8e76aa0fbc3f98075cc5cf40827c7cca6ba38c
raz_taz <98036348+raz-taz@users.noreply.github.com>
2023-04-06 21:03:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -110,6 +110,6 @@ socketIO.on('connection', (socket) => {
 
 
 
-http.listen(PORT, 'https://chat-service-api.onrender.com', () => {
+http.listen(PORT, 'localhost', () => {
     console.log(`Server listening on ${PORT}`);
 });
